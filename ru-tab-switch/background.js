2aaede6c66733a0c210eaf70535651e486055673
rstar2 <rstar2@abv.bg>
2019-02-18 10:51:18 +0200

    Finish TabSwtich

--- ru-tab-switch/background.js
+++ ru-tab-switch/background.js
@@ -1,3 +1,44 @@
+const windows = new Map();
+
+function tabSwitch() {
+    chrome.tabs.query({ currentWindow: true }, tabs => {
+        if (tabs.length === 0 || tabs.length === 1) {
+            // nothing to switch
+            return;
+        }
+
+        let lastActiveTabId = null;
+
+        // if just 2 tabs - switch them
+        if (tabs.length === 2) {
+            const notActiveTab = tabs.find(tab => !tab.active);
+            if (notActiveTab) {
+                lastActiveTabId = notActiveTab.id;
+            }
+        } else {
+            const windowId = tabs[0].windowId;
+            const windowData = windows.get(windowId);
+
+            // validate 'windowData' again as all events are async and such window acn already have been closed
+            if (windowData) {
+                lastActiveTabId = windowData.lastActiveTabId;
+                // check if there's still such tab
+                if (-1 === tabs.findIndex(tab => tab.id === lastActiveTabId)) {
+                    console.log('Window\'s data is obsolete');
+                }
+            } else {
+                console.log('Window\'s data is already cleared');
+            }
+        }
+
+        if (null != lastActiveTabId) { // Node id 0 is valid ID
+            chrome.tabs.update(lastActiveTabId, { active: true, highlighted: true });
+        } else {
+            console.log('No tab to switch to');
+        }
+    });
+}
+
 chrome.commands.onCommand.addListener(function (command) {
     console.log('Command:', command);
 
@@ -6,40 +47,28 @@ chrome.commands.onCommand.addListener(function (command) {
     tabSwitch();
 });
 
+function listenOnWindows() {
+    // listen for window removal (no need for created - this will be in the tabs)
+    chrome.windows.onRemoved.addListener(windowId => {
+        windows.delete(windowId);
+    });
+}
 
 function listenOnTabs() {
-    // Note: Currently active tab can be updated with a new URL
-    let checkTabId = -1;
-
-    // listen for when active tab is 'ready'
+    // listen for tab 'activated'
     chrome.tabs.onActivated.addListener(({ tabId, windowId }, props) => {
-        checkTabId = tabId;
-        checkContextMenu(checkTabId);
-    });
-
-    // check initially the current tab
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        checkTabId = tabs[0].id;
-        checkContextMenu(checkTabId);
+        const windowData = windows.get(windowId);
+        if (!windowData) {
+            windows.set(windowId, {
+                activeTabId: tabId
+            });
+        } else if (windowData.activeTabId !== tabId) {
+            windowData.lastActiveTabId = windowData.activeTabId;
+            windowData.activeTabId = tabId;
+        }
     });
 }
 
+listenOnWindows();
 listenOnTabs();
 
-let latestTab;
-function tabSwitch() {
-    if (latestTab) {
-        chrome.tabs.query({ currentWindow: true }, function (tabs) {
-            // Sort tabs according to their index in the window.
-            tabs.sort((a, b) => { return a.index < b.index; });
-            let activeIndex = tabs.findIndex((tab) => { return tab.active; });
-            let lastTab = tabs.length - 1;
-            let newIndex = -1;
-            if (command === 'flip-tabs-forward')
-                newIndex = activeIndex === 0 ? lastTab : activeIndex - 1;
-            else  // 'flip-tabs-backwards'
-                newIndex = activeIndex === lastTab ? 0 : activeIndex + 1;
-            chrome.tabs.update(tabs[newIndex].id, { active: true, highlighted: true });
-        });
-    }
-}
